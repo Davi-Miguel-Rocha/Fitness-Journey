dc918a30970dc7bd63adcb7a04f40e9385b112d2
MiguelLeme <142456441+MiguelLeme@users.noreply.github.com>
2025-09-17 13:23:50 -0300

    Update App.js

--- front-end/screens/App.js
+++ front-end/screens/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import StepCounter from '../components/StepCounter';
+import React, { useState } from 'react';
+import SignUpScreen from './SignUpScreen'; // Ou StepCounter, dependendo de qual tela você quer como principal
+import SplashScreen from './SplashScreen'; // Importa a nova SplashScreen AJUSTAR REFERÊNCIA (ENDEREÇO)
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <StepCounter />
-    </View>
-  );
-}
+  const [isLoading, setIsLoading] = useState(true);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-     backgroundColor: '#ffffff',
-  },
+  // Função chamada pela SplashScreen quando o carregamento termina
+  const handleFinishLoading = () => {
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    // Se ainda estiver carregando, mostra a SplashScreen
+    return <SplashScreen onFinishLoading={handleFinishLoading} />;
+  }
+
+  // Se o carregamento terminou, mostra a tela principal (neste caso, SignUpScreen)
+  return <SignUpScreen />; 
+}
 });
